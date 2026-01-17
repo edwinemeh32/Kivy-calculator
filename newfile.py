48e6198ed3a2f476854634d969378e1c79ba278d
edwinemeh32 <edwinemeh32@gmail.com>
2026-01-17 22:28:45 +0100

    Add calculator code and buildozer.spec

---
+++ newfile.py
@@ -0,0 +1,21 @@
+[app]
+title = Calculator
+package.name = calculator
+package.domain = org.example
+
+source.dir = .
+source.include_exts = py
+
+version = 0.1
+
+requirements = python3,kivy
+
+orientation = portrait
+
+fullscreen = 0
+
+android.permissions =
+
+[buildozer]
+log_level = 2
+warn_on_root = 1
